cd8b539c28b5cae3cff9b28bc065efbaf51ff3e4
msirazu <thebdrazu@gmail.com>
2026-01-20 15:25:43 +0600

    dynamic product link added

--- src/pages/products/HomeProductsList.jsx
+++ src/pages/products/HomeProductsList.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 const HomeProductsList = ({ product }) => {
-    const { title, price, discountPrice, imageUrl } = product; 
+    const { title, price, discountPrice, imageUrl, id } = product; 
     return (
-        <div className="border border-gray-300 p-2">
+        <Link to={`/product-details/${id}`}>
+        <div className="border border-gray-300 p-2 hover:border hover:border-secondary hover:cursor-pointer h-100">
             <img className="" src={imageUrl}/>
             <h2 className="text-lg font-bold">{title}</h2>
             <div className="flex justify-between my-5">
@@ -9,6 +12,7 @@ const HomeProductsList = ({ product }) => {
                 <p className="text-xl line-through">{price}</p>
             </div>
         </div>
+        </Link>
     );
 };
 
