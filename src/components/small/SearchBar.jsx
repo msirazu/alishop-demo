593b266eaf2ab9fc81bb890687ff80ad43ce7a62
msirazu <thebdrazu@gmail.com>
2026-01-21 18:32:45 +0600

    filter added on checkout page

--- src/components/small/SearchBar.jsx
+++ src/components/small/SearchBar.jsx
@@ -1,22 +1,31 @@
 const SearchBar = () => {
     return (
-        <div>
-            <label className="input">
-  <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-    <g
-      strokeLinejoin="round"
-      strokeLinecap="round"
-      strokeWidth="2.5"
-      fill="none"
-      stroke="currentColor"
-    >
-      <circle cx="11" cy="11" r="8"></circle>
-      <path d="m21 21-4.3-4.3"></path>
-    </g>
-  </svg>
-  <input type="search" required placeholder="Search" />
-</label>
-        </div>
+        <div className="w-full">
+      <div className="relative w-full">
+        <svg
+          className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 opacity-50"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+        >
+          <g
+            strokeLinejoin="round"
+            strokeLinecap="round"
+            strokeWidth="2.5"
+            fill="none"
+            stroke="currentColor"
+          >
+            <circle cx="11" cy="11" r="8" />
+            <path d="m21 21-4.3-4.3" />
+          </g>
+        </svg>
+
+        <input
+          type="search"
+          placeholder="Search"
+          className="input input-bordered w-full pl-10"
+        />
+      </div>
+    </div>
     );
 };
 
--- src/pages/shop/Checkout.jsx
+++ src/pages/shop/Checkout.jsx
@@ -1,7 +1,15 @@
+import { use } from "react";
+import DataContext from "../../auth/context/DataContext";
+import { getStoredCartProduct } from "../../utilities/addToLocalStorageDatabase";
+
 const Checkout = () => {
+    const data = use(DataContext);
+    const localStorageData = getStoredCartProduct();
+    const cartProduct = data.filter(product => localStorageData.includes(product.id))
+ 
     return (
-        <div>
-            checkout page
+        <div className="h-screen">
+            total products: {cartProduct.length}
         </div>
     );
 };
