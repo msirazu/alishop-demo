95cc90ee98d30e2d9a63eadbc8274db223c1aaed
msirazu <thebdrazu@gmail.com>
2026-01-22 09:57:24 +0600

    checkout product added from local storage

--- src/pages/shop/Checkout.jsx
+++ src/pages/shop/Checkout.jsx
@@ -5,11 +5,28 @@ import { getStoredCartProduct } from "../../utilities/addToLocalStorageDatabase"
 const Checkout = () => {
     const data = use(DataContext);
     const localStorageData = getStoredCartProduct();
-    const cartProduct = data.filter(product => localStorageData.includes(product.id))
- 
+    const cartProduct = data.filter(product => localStorageData.includes(product.id));
+    console.log(cartProduct)
     return (
-        <div className="h-screen">
-            total products: {cartProduct.length}
+        <div className="">
+            {cartProduct.map(p => 
+                <div key={p.id} className="flex items-center border border-gray-200 gap-5 m-5 p-3 font-poppins hover:border-secondary hover:cursor-pointer">
+                    <div className="">
+                        <img className="w-25" src={p.imageUrl}/>
+                    </div>
+                    <div>
+                        <h2 className="font-bold">{p.title}</h2>
+                        <p className="">
+                            <span className="line-through">
+                                {`৳${p.price}`}
+                            </span>
+                            <span className="font-bold text-xl text-third">
+                                {`৳${p.discountPrice}`}
+                            </span>
+                        </p>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
